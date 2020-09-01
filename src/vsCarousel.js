dc05452862556e17377afa3d691ea79cb7211e82
iocdacc <iocdacc@gmail.com>
2020-09-02 03:07:04 +0800

    demo修改

--- src/vsCarousel.js
+++ src/vsCarousel.js
@@ -12,7 +12,7 @@ console.log(
   '\n%c VsCarousel by%c https://blog.iocdacc.com \n',
   'color: #fff;background-image: linear-gradient(90deg, rgb(47, 172, 178) 0%, rgb(45, 190, 96) 100%);padding:5px 1px;',
   'color: #fff;background-image: linear-gradient(90deg, rgb(45, 190, 96) 0%, rgb(255, 255, 255) 100%);padding:5px 0;width: 200px;display: inline-block;'
-);
+); 
 
 export default function VsCarousel(...arg){
   this.config = {}
